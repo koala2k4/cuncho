5b054d8c3771416fb93d77bddd244f9dbfede898
koala2k4 <59249480+koala2k4@users.noreply.github.com>
2022-03-08 23:16:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: " Chúc ngủ ngon",
+    introTitle: 'Gửi con chó xấu xí cute',
+    introDesc: `Chúc con chó 364 ngày còn lại luôn luôn vui vẻ hơn 8/3.
+		Và chúc chó đậu đại học nguyện vọng 1 để trong tương lai trở thành 1 giáo viên gương mẫu 
+	`,
+    btnIntro: '^^LUV^^',
+    title: 'Lun iu con chó 🥰',
+    desc: ' Chó iu An không ??????????',
+    btnYes: 'Lun iu An <33',
+    btnNo: 'Nấu nầu :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Chó lại yêu An <3',
+    btnReply: 'Trả lời câu hỏi <3',
+    reply: 'Không iu mà phải là gấc iu <3',
+    mess: 'Thôi đừng xạo 🥰. Iu nhìu không An tự biết 😘😘',
+    messDesc: 'Gửi stk đây nào',
+    btnAccept: 'Không đồng ý không nàm người <3',
+    messLink: 'https://www.messenger.com/t/100037853323719' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}	
